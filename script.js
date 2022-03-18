bbfaf2e50966286d1b2a8f224c4bc83e9698e33f
Otohik0 <gryaznowa.katyu@yandex.ru>
2022-03-18 13:20:43 +0300

    add

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Урок по JavaScript";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 10;
+let rollback = 50;
+let fullPrice = 50000;
+let adaptive = false;
 
-alert("Hello");
-console.log("Привет, Мир!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость вёрстки экранов", screenPrice, "рублей/ долларов/гривен/юани")
+console.log("Стоимость разработки сайта", fullPrice, "рублей/ долларов/гривен/юани")
+
+console.log(screens.toLowerCase())
+console.log(screens.split(", "))
+
+console.log(fullPrice * (rollback/100))
+
+// alert("Hello");
+// console.log("Привет, Мир!");
