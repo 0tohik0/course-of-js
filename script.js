965120f39912f9f6427be654d9e76164921e59ef
Otohik0 <gryaznowa.katyu@yandex.ru>
2022-03-22 12:05:13 +0300

    add

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ let servicePrice2 = +prompt("Сколько это будет стоить?");
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - rollback / 100);
 
-const showTypeOf = function(variable) {
+const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
 };
 
-const getRollbackMessage = function(price) {
+const getRollbackMessage = function (price) {
     if (price >= 30000) {
         return "Даём скидку в 10%";
     } else if (price >= 15000 && price < 30000) {
@@ -28,30 +28,29 @@ const getRollbackMessage = function(price) {
     }
 }
 
-const getAllServicePrices = function() {
+const getAllServicePrices = function () {
     return servicePrice1 + servicePrice2;
 };
-fullPrice = getAllServicePrices();
 
-const getFullPrice = function() {
-   return screenPrice + fullPrice;
+const getFullPrice = function () {
+   return screenPrice + getAllServicePrices;
 };
-    fullPrice = getFullPrice();
 
-const getTitle = function() {
+const getTitle = function () {
     let titleNew = title.split('')[0].toUpperCase()
     let title2 = title.slice(1);
     title = titleNew + title2
     return title
 }; 
-    title = getTitle();
 
-const getServicePercentPrices = function() {
+const getServicePercentPrices = function () {
     return fullPrice - (fullPrice * (rollback / 100));
 };
-    servicePercentPrice = getServicePercentPrices();
 
-
+fullPrice = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
 showTypeOf(title);
 showTypeOf(screenPrice);
