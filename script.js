1218bf2f36c1636feeb263068880595f95f0b8fb
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-04-08 12:28:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,21 +10,22 @@ let service2 = prompt('Какой дополнительный тип услуг
 let servicePrice2 = +prompt('Сколько это будет стоить?', '16000');
 
 let rollback = 20;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+// let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+// let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
+let fullPrice = -15000;
+console.log(fullPrice);
 
-if (fullPrice > 30000) {
+if (fullPrice >= 30000) {
     console.log('Даем скидку в 10%');
-} else if ((30000 > fullPrice) && (fullPrice > 15000)) {
+} else if ((fullPrice < 30000) && (15000 <= fullPrice)) {
     console.log('Даем скидку в 5%');
-} else if ((15000 > fullPrice) && (fullPrice > 0)) {
+} else if ((fullPrice < 15000) && (0 <= fullPrice)) {
     console.log('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
+} else {
     console.log('Что то пошло не так');
-}
+};
 
 
-
-console.log(servicePercentPrice);
+// console.log(servicePercentPrice);
 
