7c57469c434a35b9963cda7f4d38b5bf1fbe8216
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-04-09 13:47:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 'use strict';
 
-let title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные");
-let screenPrice = +prompt("Сколько будет стоить данная работа?", 16000);
-let rollback = Math.random() * 50;
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice - rollback / 100);
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let rollback = 20;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let service1;
+let service2;
 
-const getAllServicePrices = function () {
-    let sum = 0;
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
 
-    for (let i = 0; i < 2; i++) {
-        sum += +prompt("Какой дополнительный тип услуги нужен?");
+const asking = function () {
+    title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+
+    screenPrice = prompt('Сколько будет стоить данная работа?');
+
+    while(!isNumber(screenPrice)) {
+        screenPrice = prompt('Сколько будет стоить данная работа?');
     }
 
-    return  sum 
+    adaptive = confirm('Нужен ли адаптив на сайте?');
+
+}
+
+const getAllServicePrices = function () {
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+
+        if (i === 0) {
+            prompt('Какой дополнительный тип услуги нужен?', 'Метрика');
+        } else if (i === 1) {
+            prompt('Какой дополнительный тип услуги нужен?', 'Отправка формы');
+        }
+
+        sum += +prompt('Сколько это будет стоить?');
+    }
+
+    return sum
     // return servicePrice1 + servicePrice2;
 };
 
@@ -28,43 +52,43 @@ const showTypeOf = function (variable) {
 };
 
 const getFullPrice = function () {
-   return screenPrice + getAllServicePrices;
+    return screenPrice + allServicePrices;
 };
-
-const getTitle = function () {
-    let titleNew = title.split('')[0].toUpperCase()
-    let title2 = title.slice(1);
-    title = titleNew + title2
-    return title
-}; 
 
 const getServicePercentPrices = function () {
     return fullPrice - (fullPrice * (rollback / 100));
 };
 
-const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return "Даём скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-        return "Даём скидку в 5";
-    } else if (price >= 0 && price < 15000) {
-        return "Скидка не предусмотрена";
-    } else {
-        return "Что-то пошло не так";
-    }
-}
+const getTitle = function () {
+    return title.trim()[0].toUpperCase() + title.trim().slice(1);
+};
 
-AllServicePrices = getAllServicePrices();
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+       return 'Даем скидку в 10%';
+    } else if (30000 > price && price >= 15000) {
+        return 'Даем скидку в 5%';
+    } else if (15000 > price && price >= 0) {
+        return 'Скидка не предусмотрена';
+    } else {
+        return 'Что то пошло не так';
+    };
+}; 
+
+asking();
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrices();
 title = getTitle();
+servicePercentPrice = getServicePercentPrices();
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log("AllServicePrices", AllServicePrices)
+console.log('allServicePrices', allServicePrices);
 
-console.log("Следующие типы экранов: " + screens);
 console.log(getRollbackMessage(fullPrice));
-console.log("Cтоимость за вычетом процента отката посреднику:" + Math.round(servicePercentPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
