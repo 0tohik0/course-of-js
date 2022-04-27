b0e6a7d9c031c76b4302aaafc13cfc921fd3043a
0tohik0 <gryaznowa.katyu@yandex.ru>
2022-04-27 14:45:19 +0300

    refact

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num) && num != 0
 }
 
-const one = function () {
-    let randomVariant = Math.floor(Math.random() * 100)
+const numberRun = function () {
+    let randomNumber = Math.floor(Math.random() * 100)
     let count = 10
 
-    console.log(randomVariant);
+    console.log(randomNumber);
 
-    const two = function () {
-        let variant = +prompt("Угадай число от 1 до 100")
+    const numberInput = function () {
+        let numb = +prompt('Угадай число от 1 до 100')
         count--
-        if (variant == randomVariant) {
-            const again = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")
+        if (numb == randomNumber) {
+            const again = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
             if (again) {
-                one()
+                numberRun()
             } else {
                 alert('Игра окончена.')
             }
-        } else if (count < 1) {
+        } 
+        else if (count < 1) {
             const again = confirm('Попытки закончились. Хотите сыграть еще?')
             if (again) {
-                one()
+                numberRun()
             } else {
                 alert('Игра окончена.')
             }
-        } else if (variant === null) {
+        } 
+        else if (numb === null) {
             alert('Игра окончена.')
-        } else if (!isNumber(variant)) {
+        } 
+        else if (!isNumber(numb)) {
             alert('Введи число!')
             count++
-            two()
-        } else if (variant > 100) {
-            alert('Введи число меньше ста!')
-            count++
-            two()
-        } else if (variant < randomVariant) {
+            numberInput()
+        } 
+        else if (numb < randomNumber) {
             alert('Загаданное число больше, осталось попыток: ' + count)
-            two()
-        } else if (variant > randomVariant) {
+            numberInput()
+        } 
+        else if (numb > randomNumber) {
             alert('Загаданное число меньше, осталось попыток: ' + count)
-            two()
+            numberInput()
         }
     }
-    two()
+    numberInput()
 }
 
-one()
-
+numberRun()
