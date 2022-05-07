fd5e78d3dd2671a5760a97bd9192f44a062c8e2f
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-05-07 15:01:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict';
 
-alert ('Здравствуй, Дружочек-Пирожочек!');
+const button = document.getElementById('btn');
+const square = document.getElementById('square');
+const inputText = document.querySelector('[type="text"]')
+const e_button = document.getElementById('e_btn')
+const inputRange = document.querySelector('[type="range"]')
+const circle = document.getElementById('circle')
+const spanRange = document.getElementById('range-span')
 
-console.log('Я текст в консольки :3');
+button.addEventListener('click', function() {
+    square.style.backgroundColor = inputText.value
+})
+
+e_button.addEventListener('click', function() {
+    e_button.style.display = 'none'
+})
+
+const logger = function (e) {
+    spanRange.textContent = e.target.value
+}
+
+const changeWH = function (e) {
+    circle.style.width = e.target.value + '%'
+    circle.style.height = e.target.value + '%'
+}
+
+inputRange.addEventListener('input', logger)
+inputRange.addEventListener('input', changeWH)
+
+
+
+
+
+
