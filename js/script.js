04bf81652d4c9ecef6df096344f198c63f2fccb7
0tohik0 <71322445+0tohik0@users.noreply.github.com>
2022-05-08 00:35:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const todoControl = document.querySelector('.todo-control')
 const headerInput = document.querySelector('.header-input')
 const todoList = document.querySelector('.todo-list')
 const todoCompleted = document.querySelector('.todo-completed')
+const todoContainer = document.querySelector(".todo-container")
 
 const toDoData = []
 
@@ -30,24 +31,46 @@ const render = () => {
                 item.completed = !item.completed
                 render()
             })
+
+            li.querySelector(".todo-remove").addEventListener("click", function () {
+                li.remove()
+                toDoData.splice(i, 1)
+                render()
+                localStorage.setItem("todoList", JSON.stringify(toDoData))
+                if (toDoData.length === 0) {
+                  localStorage.clear()
+                }
+            })
+            if (toDoData.length >= 1) {
+                localStorage.setItem("todoList", JSON.stringify(toDoData))
+        }
     })
 }
-
-todoControl.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const newToDo = {
-        text: headerInput.value,
-        completed: false
-    }
-
-    toDoData.push(newToDo)
-    headerInput.value = ""
-
+const check = () => {
+    toDoData = localStorage.getItem("todoList")
+      ? JSON.parse(localStorage.getItem("todoList"))
+      : []
     render()
+}
 
-})
-
+todoControl.addEventListener("submit", function (event) {
+    event.preventDefault()
+  
+    headerInput.value = headerInput.value.trim()
+  
+    if (headerInput.value.length !== 0) {
+      const newToDo = {
+        text: headerInput.value,
+        completed: false,
+      }
+  
+      toDoData.push(newToDo)
+  
+      headerInput.value = ""
+      render()
+    }
+  })
+  check()
 
 
 
