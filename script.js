5d2950df2ae2bd255fdddc6aaacbc7bd2790ce17
0tohik0 <gryaznowa.katyu@yandex.ru>
2022-05-08 19:14:01 +0300

    add

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict'
 
-alert ('Здравствуй, Дружочек-Пирожочек!');
+function DomElement (selector, height, width, bg, fontSize) {
+    this.selector = selector
+    this.height = height
+    this.width = width
+    this.bg = bg
+    this.fontSize = fontSize
+}
 
-console.log('Я текст в консольке :3');
+DomElement.prototype.newElem = function () {
+    let elem
+    if (this.selector[0] === '.') {
+        elem = document.createElement('div')
+        elem.className = this.selector.slice(1)
+    }
+    if (this.selector[0] === '#') {
+        elem = document.createElement('p')
+        elem.id = this.selector.slice(1)
+    }
+    elem.style.cssText = `height: ${this.height}px;
+        width: ${this.width}px;
+        background: ${this.bg};
+        font-size: ${this.fontSize}px;`
+    return elem
+}
+
+let obg1 = new DomElement('.block', 100, 200, 'blue', 12)
+let obg2 = new DomElement('#best', 100, 200, 'red', 16)
+
+document.body.appendChild(obg1.newElem())
+document.body.appendChild(obg2.newElem())
